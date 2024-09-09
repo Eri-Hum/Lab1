97b8698c6b4d412adac26cd952548ddf2b286514
Erik Hummer <erikhummer@AirsomtlhorErik.lan>
2024-09-09 10:40:08 +0200

    implementing/testing contact-form with some basic JS

---
+++ contact-form.js
@@ -0,0 +1,7 @@
+function sendMail (){
+    var subject = document.getElementById('subject').value;
+    var body = document.getElementById('body').value;
+
+    console.log("This is the subject:" + subject + "This is the text:" + body);
+
+}
